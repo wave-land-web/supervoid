1fb89778c843a8b68d257d4ec77082f5de8b1f2a
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2024-07-31 14:48:20 -0500

    feat(app): set up font sizes, colors, basic styles and routing

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,10 +6,34 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        header: ['SF Mono Regular', ...defaultTheme.fontFamily.serif],
-        body: ['Rotonto Regular', ...defaultTheme.fontFamily.sans],
+        header: ['Rotonto Regular', ...defaultTheme.fontFamily.serif],
+        body: ['SF Mono Regular', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    fontSize: {
+      xs: ['var(--fs-xs)', { lineHeight: 'var(--line-height)' }],
+      base: ['var(--fs-base)', { lineHeight: 'var(--line-height)' }],
+      sm: ['var(--fs-s)', { lineHeight: 'var(--line-height)' }],
+      m: ['var(--fs-m)', { lineHeight: 'var(--line-height)' }],
+      l: ['var(--fs-l)', { lineHeight: 'var(--line-height)' }],
+      xl: ['var(--fs-xl)', { lineHeight: 'var(--line-height)' }],
+      '2xl': ['var(--fs-xxl)', { lineHeight: 'var(--line-height)' }],
+      '3xl': ['var(--fs-xxxl)', { lineHeight: 'var(--line-height)' }],
+      '4xl': ['var(--fs-xxxxl)', { lineHeight: 'var(--line-height)' }],
+      '5xl': ['var(--fs-xxxxxl)', { lineHeight: 'var(--line-height)' }],
+      '6xl': ['var(--fs-xxxxxxl)', { lineHeight: 'var(--line-height)' }],
+      '7xl': ['var(--fs-xxxxxxxl)', { lineHeight: 'var(--line-height)' }],
+      '8xl': ['var(--fs-xxxxxxxxl)', { lineHeight: 'var(--line-height)' }],
+      '9xl': ['var(--fs-xxxxxxxxxl)', { lineHeight: 'var(--line-height)' }],
+    },
+    colors: {
+      white: 'var(--color-white)',
+      black: 'var(--color-black)',
+      magenta: {
+        900: 'var(--color-magenta-900)',
+        500: 'var(--color-magenta-500)',
+        100: 'var(--color-magenta-100)',
       },
     },
   },
-  plugins: [],
 }
