6c904dc1e04f4e82ec63c2725f51efb90a815c8e
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2025-06-04 16:03:30 -0500

    feat(app): upgrade astro, tailwind + swiper, col styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,17 @@ import mdx from '@astrojs/mdx'
 import netlify from '@astrojs/netlify'
 import partytown from '@astrojs/partytown'
 import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.supervoid.tv',
-  output: 'hybrid',
   prefetch: {
     prefetchAll: true,
   },
   integrations: [
-    tailwind({
-      nesting: true,
-    }),
     icon(),
     sitemap({
       lastmod: new Date(),
@@ -30,5 +26,45 @@ export default defineConfig({
   }),
   devToolbar: {
     enabled: true,
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    // SEE: https://docs.astro.build/en/reference/experimental-flags/fonts/#local-font-variants
+    fonts: [
+      {
+        provider: 'local',
+        name: 'Rotonto Regular',
+        cssVariable: '--font-header',
+        variants: [
+          {
+            src: [
+              './src/assets/fonts/rotonto-regular.woff2',
+              './src/assets/fonts/rotonto-regular.woff',
+            ],
+            weight: '400',
+            style: 'normal',
+            display: 'block',
+          },
+        ],
+      },
+      {
+        provider: 'local',
+        name: 'SF Mono Regular',
+        cssVariable: '--font-body',
+        variants: [
+          {
+            src: [
+              './src/assets/fonts/sf-mono-regular.woff2',
+              './src/assets/fonts/sf-mono-regular.woff',
+            ],
+            weight: '400',
+            style: 'normal',
+            display: 'block',
+          },
+        ],
+      },
+    ],
   },
 })
