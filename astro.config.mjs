cf7481776b23d9c21b6fb1659c7e759814341e77
Josh Nussbaum <joshnussbaum89@gmail.com>
2026-08-08 13:18:43 -0500

    (perf): enhance video player integration by adding Mux and YouTube options in Layout component, responsive images

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ import icon from 'astro-icon'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.supervoid.tv',
+  image: {
+    layout: 'constrained',
+  },
   prefetch: {
     prefetchAll: true,
   },
